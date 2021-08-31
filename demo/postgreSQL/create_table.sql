29c84952f7da8070075498a5b79ea98e36bc6c0f
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2021-08-31 22:01:33 +0200

    demo

--- demo/postgreSQL/create_table.sql
+++ demo/postgreSQL/create_table.sql
@@ -6,6 +6,8 @@ CREATE SCHEMA app;
 -- 	MAXVALUE 9223372036854775807
 -- 	START 1;
 
+DROP TABLE app.customer;
+
 CREATE TABLE IF NOT EXISTS app.customer (
 	-- id integer NOT NULL DEFAULT nextval('app.customer_id_seq'),
 	id integer NOT NULL,
@@ -14,6 +16,9 @@ CREATE TABLE IF NOT EXISTS app.customer (
 	city varchar(100) NULL,
     active bool DEFAULT 't',
     category integer DEFAULT 1,
+    hashtag varchar(20),
 	created timestamp NULL,
+    inserted timestamp NULL DEFAULT now(),
+    lastUpdate timestamp NULL,
 	CONSTRAINT customer_pkey PRIMARY KEY (id)
 );
